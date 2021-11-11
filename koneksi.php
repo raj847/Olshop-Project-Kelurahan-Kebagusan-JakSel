f1821dccc378271eead905c2cca4fccba288d71c
raj847 <aryadevaraj19@gmail.com>
2021-11-11 21:40:05 +0700

    new1

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,11 @@
 <?php
 
-$koneksi = new mysqli("localhost", "root", "", "db_tokoonline");
+$host = "remotemysql.com";
+$db ="DWqDe09S9e";
+$user="DWqDe09S9e";
+$pass="eJiuG3DuZM";
+// $charset=" ";
+
+$koneksi = new mysqli("remotemysql.com", "DWqDe09S9e", "eJiuG3DuZM", "DWqDe09S9e");
+
+?>
